e39d357f9bc1be69602b734dd57686a4b0ce1840
muca1533 <127457551+muca1533@users.noreply.github.com>
2023-12-08 20:24:04 -0300

    081223

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    
+    if(elemento != null && elemento.localName === 'audio') {
+      elemento.play();
+    }
+    
+  else{
+    console.log('elemento não encontrado ou seletor inválido');
+    
+   }
+
 }
 
-document.querySelectorAll('.tecla') 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+
+       
+        if (evento.code === 'Space' ||evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+
+
+    }
+    
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+
+
+}
